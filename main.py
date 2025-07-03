08257505fa8181b1e3719d6f27887fde259bb441
Jose carlos <solracosej8731@gmail.com>
2025-07-03 10:00:24 -0300

    Reestruturando projeto

--- main.py
+++
@@ -1,5 +0,0 @@
-from view.tela_principal_view import TelaPrincipal
-
-if __name__ == "__main__":
-    app = TelaPrincipal()
-    app.executar()
--- models/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def conector():
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='984641940',
-        database='restaurante'
-    )
